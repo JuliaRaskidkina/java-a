eec93ef8aa169c71dc950341dd6d241192fb8b1e
JuliaRaskidkina <32374151+JuliaRaskidkina@users.noreply.github.com>
2017-09-29 17:01:48 +0300

    Add files via upload

---
+++ CircleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by user on 29.09.2017.
+ */
+class CircleTest {
+    @Test
+    void area() {
+    }
+
+}
